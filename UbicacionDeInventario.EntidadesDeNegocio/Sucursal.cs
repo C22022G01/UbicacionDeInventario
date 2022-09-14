98b7765da1f9716470b442e54014c49a409142ae
Edgar Wilfredo Hernandez Perez <hp22005@esfe.agape.edu.sv>
2022-09-14 14:31:03 -0300

    Se programo la Clase Sucursal.cs

---
+++ UbicacionDeInventario.EntidadesDeNegocio/Sucursal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ********************************
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UbicacionDeInventario.EntidadesDeNegocio
+{
+    public class Sucursal
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Telefono es obligatorio")]
+        [StringLength(10, ErrorMessage = "Maximo 10 caracteres")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "Direccion es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Comentario { get; set; }
+        [Required(ErrorMessage = "Direccion es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+
+        [NotMapped]
+        public int Top_Aux { get; set; }
+       
+    }
+}
