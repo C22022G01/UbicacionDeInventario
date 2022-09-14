0ad2d9ff8790c68f63ba3110ba7901d6952bb15c
Cristian Emilio Carias palacios <cp22005@esfe.agape.edu.sv>
2022-09-14 10:35:10 -0600

    programada la capa de logica de negocio la claee bodega

--- UbicacionDeInventario.ProebasUnitarias/BodegaDALTests.cs
+++ UbicacionDeInventario.ProebasUnitarias/BodegaDALTests.cs
@@ -6,45 +6,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**********************************/
+using UbicacionDeInventario.EntidadesDeNegocio;
+
 namespace UbicacionDeInventario.AccesoADatos.Tests
 {
     [TestClass()]
     public class BodegaDALTests
     {
+        private static Bodega bodegaInicial = new Bodega { Id = 2 };  // Agregar un id existente en la base de datos     
         [TestMethod()]
-        public void CrearAsyncTest()
+        public async Task T1CrearAsyncTest()
         {
-            Assert.Fail();
+            var bodega = new Bodega();
+            bodega.IdSucursal = bodegaInicial.IdSucursal;
+            bodega.Nombre = "Administrador";
+            bodegaInicial.Estatus = (byte)Estatus_Bodega.INACTIVO;
+            bodega.Descripcion = "Test Descripcion;
+            int result = await BodegaDAL.CrearAsync(bodega);
+            Assert.AreNotEqual(0, result);
+            bodegaInicial.Id = bodega.Id;
         }
 
         [TestMethod()]
-        public void ModificarAsyncTest()
+        public async Task T2ModificarAsyncTest()
         {
-            Assert.Fail();
+            var bodega = new Bodega();
+            bodega.Id = bodegaInicial.Id;
+            bodega.IdSucursal = bodegaInicial.IdSucursal;
+            bodega.Nombre = "Admin";
+            bodegaInicial.Estatus = (byte)Estatus_Bodega.INACTIVO;
+            bodega.Descripcion = "Test Descripcion;
+            int result = await BodegaDAL.ModificarAsync(bodega);
+            Assert.AreNotEqual(0, result);
+
         }
 
         [TestMethod()]
-        public void EliminarAsyncTest()
+        public async Task T3ObtenerPorIdAsyncTest()
         {
-            Assert.Fail();
+            var rol = new Rol();
+            rol.Id = rolInicial.Id;
+            var resultRol = await RolDAL.ObtenerPorIdAsync(rol);
+            Assert.AreEqual(rol.Id, resultRol.Id);
         }
 
         [TestMethod()]
-        public void ObtenerPorIdAsyncTest()
+        public async Task T4ObtenerTodosAsyncTest()
         {
-            Assert.Fail();
+            var resultRoles = await RolDAL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultRoles.Count);
         }
 
         [TestMethod()]
-        public void ObtenerTodosAsyncTest()
+        public async Task T5BuscarAsyncTest()
         {
-            Assert.Fail();
+            var rol = new Rol();
+            rol.Nombre = "a";
+            rol.Top_Aux = 10;
+            var resultRoles = await RolDAL.BuscarAsync(rol);
+            Assert.AreNotEqual(0, resultRoles.Count);
         }
-
         [TestMethod()]
-        public void BuscarAsyncTest()
+        public async Task T6EliminarAsyncTest()
         {
-            Assert.Fail();
+            var rol = new Rol();
+            rol.Id = rolInicial.Id;
+            int result = await RolDAL.EliminarAsync(rol);
+            Assert.AreNotEqual(0, result);
         }
     }
 }
