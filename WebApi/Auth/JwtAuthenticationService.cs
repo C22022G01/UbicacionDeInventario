d539b5c83d0bee25818bbd417f913a1f8a8b3d69
Edgar Wilfredo Hernandez Perez <hp22005@esfe.agape.edu.sv>
2022-09-16 15:35:27 -0300

    cambios web ai

---
+++ WebApi/Auth/JwtAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// agregar la siguiente libreria
+using UbicacionDeInventario.EntidadesDeNegocio;
+//************************************
+
+namespace UbicacionDeInventario.WebAPI.Auth
+{
+    public interface UwtAuthenticationService
+    {
+        string Authenticate(Usuario pUsuario);
+    }
+}
+
