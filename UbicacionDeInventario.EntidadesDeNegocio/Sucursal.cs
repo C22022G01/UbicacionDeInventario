69172e04d386ad66cc1b65a5a62cb7bfc00af8e4
Natalia Flores <fm22002@esfe.agape.edu.sv>
2022-09-20 15:19:07 -0600

    otro cambio

--- UbicacionDeInventario.EntidadesDeNegocio/Sucursal.cs
+++ UbicacionDeInventario.EntidadesDeNegocio/Sucursal.cs
@@ -26,7 +26,7 @@ namespace UbicacionDeInventario.EntidadesDeNegocio
         public string Comentario { get; set; }
         [Required(ErrorMessage = "El comentario es requerido")]
         [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
-        public Bodega? Bodega { get; set; }
+        public List<Bodega>? Bodega { get; set; }
 
         [NotMapped]
         public int Top_Aux { get; set; }
