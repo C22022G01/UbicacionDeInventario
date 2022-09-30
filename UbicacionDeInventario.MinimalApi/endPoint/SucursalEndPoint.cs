3f4a7fdc63487a8c18787a330de572e63c3c8f48
Edgar Wilfredo Hernandez Perez <hp22005@esfe.agape.edu.sv>
2022-09-29 23:48:37 -0300

    Se Modifico la Clase SucursalEndPoint.cs

--- UbicacionDeInventario.MinimalApi/endPoint/SucursalEndPoint.cs
+++ UbicacionDeInventario.MinimalApi/endPoint/SucursalEndPoint.cs
@@ -1,6 +1,28 @@
-﻿namespace UbicacionDeInventario.MinimalApi.endPoint
+﻿using UbicacionDeInventario.MinimalApi.MVC;
+using UbicacionDeInventario.LogicaDeNegocio;
+using UbicacionDeInventario.EntidadesDeNegocio;
+
+namespace UbicacionDeInventario.MinimalApi.endPoint
 {
-    public class SucursalEndPoint
+    public static class SucursalEnPoint
     {
+        public static void addSucursalEndPoits(this WebApplication app)
+        {
+            // app.MapPost("/Sucursal/create", sucursalBL.CrearAsync);
+            app.MapPost("/sucursal/create", async (SucursalCrear pSucursal, SucursalBL sucursal2BL) => {
+                return await sucursal2BL.CrearAsync(new Sucursal
+                {
+                    Nombre = pSucursal.Nombre,
+                    Telefono = pSucursal.Telefono,
+                    Comentario = pSucursal.Comentario,
+                    Direccion = pSucursal.Direccion,
+                });
+            });
+            SucursalBL sucursalBL = new SucursalBL();
+            app.MapPost("/rol/delete", sucursalBL.EliminarAsync);
+            app.MapPut("/rol", sucursalBL.ModificarAsync);
+            app.MapGet("/rol", sucursalBL.ObtenerTodosAsync);
+            app.MapPost("/rol/search", sucursalBL.BuscarAsync);
+        }
     }
 }
