57b803eccbee6a868f30e3b9323884644592591e
Edgar Wilfredo Hernandez Perez <hp22005@esfe.agape.edu.sv>
2022-09-29 23:45:00 -0300

    Se Programo la clae SucursalEndPoint.cs y la clase SucursalCreate.cs tambien la clase Sucursal.razor

---
+++ UbicacionDeInventario.UI.Blazor/MVC/SucursalCreate.cs
@@ -0,0 +1,10 @@
+﻿namespace UbicacionDeInventario.UI.Blazor.MVC
+{
+    public class SucursalCreate
+    {
+        public int Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Comentario { get; set; }
+        public string Direccion { get; set; }
+    }
+}
---
+++ UbicacionDeInventario.UI.Blazor/endPoint/SucursalEndPoint.cs
@@ -0,0 +1,10 @@
+﻿using UbicacionDeInventario.EntidadesDeNegocio;
+using UbicacionDeInventario.LogicaDeNegocio;
+using UbicacionDeInventario.UI.Blazor.MVC;
+namespace UbicacionDeInventario.UI.Blazor.endPoint
+{
+    public class SucursalEndPoint
+    {
+
+    }
+}
